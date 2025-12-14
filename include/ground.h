68c5bcc55f98f49b218c4d8be991460c47c5aec6
Zhu Xiaoyao <1933570593@qq.com>
2025-12-14 10:58:44 +0800

    添加地面+树木

---
+++ include/ground.h
@@ -0,0 +1,35 @@
+#pragma once
+#include"glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/matrix_transform.hpp>
+#include "shader.h"
+
+
+
+class Ground
+{
+public:
+    Ground(float size = 1000.0f);
+    ~Ground();
+
+    bool init();
+    void render(const glm::mat4& view, const glm::mat4& projection, float yOffset);
+    // void setSize(float s);
+
+private:
+    void initGeometry();
+    //void initTexture();
+
+
+    GLuint VAO, VBO;
+    GLuint textureID;
+
+    float size;
+    //float vertices[30];   // 6 点，每点5个float (x,y,z,u,v)
+
+    Shader* shader;
+    //bool shaderInitialized;
+};
+
+//extern Ground* ground;
---
+++ include/tree.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <glad/glad.h>
+#include <glm/glm/glm.hpp>
+#include "shader.h"
+
+class Tree {
+public:
+    Tree();
+    ~Tree();
+    bool init();
+    void draw(const glm::mat4& view, const glm::mat4& projection, const glm::mat4& model);
+
+private:
+    unsigned int VAO, VBO;
+
+    void initGeometry();
+    //void initShader();
+
+    // Ê÷¸ÉÎÆÀí
+
+    unsigned int trunkTextureID;
+
+    Shader* shader;
+
+    // Ê÷Ò¶
+    // unsigned int leafTexture;
+};
